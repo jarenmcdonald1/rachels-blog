9b656ef6acfe591a2a557c3deb7e2972a37ce487
Jaren M <jaren.mcd@gmail.com>
2020-08-02 11:28:22 -0400

    updated bio subtitle; added style to 404 page

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         <Col md={8} className="d-flex justify-content-center align-items-center pl-md-2">
           <Card.Body>
             <Card.Title className="font-weight-bold text-pink" style={{fontSize: '1.95em'}}>Hi, I'm Rachel (CNP, Hons).</Card.Title>
-            <Card.Text className="h4 text-muted mb-3">I’m a Holistic Nutritionist, Professional Dancer, and Dance Instructor/Choreographer.</Card.Text>
+            <Card.Text className="h4 text-muted mb-3">I’m a Holistic Nutritionist, Professional Dancer, and Instructor/Choreographer.</Card.Text>
             <Card.Text>My whole life I have been interested in health and wellness. The passion was handed down from my mom who would send me to school with Chlorophyll water (thanks, mom!). While I was completing my Professional Intense Dance Training program I began to experience digestive issues. I would curl up into a little ball with intense abdominal pain! With no clear answer, I tried to eliminate some known allergens. I noticed an immediate improvement in heightened energy levels and stronger body. This allowed me to push my body technically and physically which took my performance to the next level.</Card.Text>
             <Card.Text>From there my passion for nutrition took the centre stage. I decided to attend The Institute of Holistic Nutrition where I would deep dive into all things health-related. I graduated and gained my Certified Nutritional Practitioner Certification (CNP, Hons.)</Card.Text>
             <Link to="/contact" className="btn btn-lg btn-info rounded-pill">Let's chat</Link>
